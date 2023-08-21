a773da966c5cc7b57d52ef51a55ac1037cb2d882
Bart Lens <bart@nextbook.io>
2023-08-21 23:56:46 +0200

    followed steps outlined in https://github.com/remix-run/remix/issues/6994#issue-1828010686

---
+++ app/root.tsx
@@ -0,0 +1,37 @@
+import { cssBundleHref } from "@remix-run/css-bundle";
+import type { LinksFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from "@remix-run/react";
+
+export const links: LinksFunction = () => [
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+];
+
+export const loader = () => ({ some: "data" });
+
+export default function App() {
+  const data = useLoaderData<typeof loader>();
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
